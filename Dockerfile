9de9418be9576d44bd21c86e8c3dcbf9302ba63c
Olivér <oliver.ocsko@dorsum.eu>
2021-03-01 10:00:09 +0100

    refact dockerfile with entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,9 @@ RUN mvn -U clean package -PskipJavaDoc
 FROM openjdk:8-jdk
 
 COPY --from=builder "/app/fifth-cucc-service/target/*-service-*.jar" service.jar
+COPY ./entrypoint.sh /
+RUN chmod 700 /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
 
 CMD [ "sh", "-c", "java -Dserver.port=$PORT -Xmx300m -Xss512k -XX:CICompilerCount=2 -Dfile.encoding=UTF-8 -XX:+UseContainerSupport -Djava.security.egd=file:/dev/./urandom -jar /service.jar" ]
--- docker-build.sh
+++ docker-build.sh
@@ -23,7 +23,12 @@ CONTAINER_NAME=fifth-service-1
 DOCKER_BUILD_PATH=.
 docker rm $(docker stop $(docker ps -aq --filter name=${CONTAINER_NAME}))
 docker system prune -f
+echo "--------------------------------------------"
+echo "------------- Create Image -----------------"
+echo "--------------------------------------------"
 docker build -t ${IMAGE_NAME} -f ./${DOCKER_BUILD_PATH}/Dockerfile ./${DOCKER_BUILD_PATH}/
+echo "-------------------------------------------------"
 echo "------------- Container Running -----------------"
+echo "-------------------------------------------------"
 #docker run --rm -it --name ${CONTAINER_NAME} -v ${LOCAL_VOLUME_PATH}:/app/properties -e ENVIRONMENT=dev -p 8081:8081 ${IMAGE_NAME}
 docker run --rm -it --name ${CONTAINER_NAME} --env PORT=9095 -p 9095:9095 ${IMAGE_NAME}
